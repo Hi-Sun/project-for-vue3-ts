f05a1b266b9e64d2e0a3d37f2d669010da653e89
sunyaling <sunyaling@58.com>
2024-06-21 19:06:41 +0800

    feat: init project

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default () => {
+  return defineConfig({
+    base: './',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 8080,
+      // proxy: {
+      //   '/api/workflows': {
+      //     target: 'http://10.253.28.47:8888',
+      //     changeOrigin: true,
+      //     // rewrite: (path) => path.replace(/^\/api/, ''),
+      //   },
+      //   '/api': {
+      //     target: 'http://10.192.17.135:8888',
+      //     changeOrigin: true,
+      //     // rewrite: (path) => path.replace(/^\/api/, ''),
+      //     // bypass(req, res, options) {
+      //     //   const realUrl = options.target + (options.rewrite ? options.rewrite(req.url) : '');
+      //     //   console.log(realUrl); // 在终端显示
+      //     //   res.setHeader('A-Real-Url', realUrl); // 添加响应标头(A-Real-Url为自定义命名)，在浏览器中显示
+      //     // },
+      //   },
+      // }
+    },
+  })
+}
