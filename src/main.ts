9d5358761f0969d15f40fafb83dc948584b97f9e
sunyaling <sunyaling@58.com>
2025-02-19 11:00:38 +0800

    feat: 添加规范相关配置

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import router from './router'
-import App from './App.vue'
-import './index.css'
+import {createApp} from "vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import router from "./router";
+import App from "./App.vue";
+import "./index.css";
 
-const app = createApp(App)
-app.use(ElementPlus)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(ElementPlus);
+app.use(router);
+app.mount("#app");
